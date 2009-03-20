8854d9546d23455502413b9b351e40cfc29093be
Matthew Garrett <mattga@gmail.com>
2009-03-19 22:42:08 -0500

    Moving a bunch of stuff around.

--- rakefile.rb
+++ rakefile.rb
@@ -1,15 +1,16 @@
 task :build do
   secretary = Sprockets::Secretary.new(
     :asset_root   => "public",
-    :load_path    => ["vendor/sprockets/*/src", "vendor/plugins/*/javascripts"],
+    :load_path    => ["vendor/sprockets/*/src", "vendor/prototype", "vendor/scriptaculous", "vendor/plugins/*/javascripts"],
     :source_files => ["app/javascripts/palette.js", "app/javascripts/**/*.js"]
   )
-
+  
   # Generate a Sprockets::Concatenation object from the source files
   concatenation = secretary.concatenation
+  
   # Write the concatenation to disk
-  concatenation.save_to("public/application.js")
-
+  concatenation.save_to("public/javascripts/application.js")
+  
   # Install provided assets into the asset root
   secretary.install_assets
 end
