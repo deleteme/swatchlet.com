efbe34b5b8b5f173b603378fd2d13a63625188b6
Matthew Garrett <mattga@gmail.com>
2009-03-22 11:42:48 -0500

    Undoing some crap.

--- public/javascripts/links.js
+++ public/javascripts/links.js
@@ -3,17 +3,7 @@ var Links = {
   setup: function(){
     this.links = $('links');
     
-    // this.links.observe('click', function(e){
-    //   console.log(e);
-    //   if ($(e.target).readAttribute('href').include('#')) {
-    //     $(e.target).fire('link:clicked', { href: $(e.target).readAttribute('href') });
-    //     $(e.target).blur();
-    //     e.stop();
-    //   }
-    // });
-    
-    $('add').observe('click', function(e){
-      console.log(e);
+    this.links.observe('click', function(e){
       if ($(e.target).readAttribute('href').include('#')) {
         $(e.target).fire('link:clicked', { href: $(e.target).readAttribute('href') });
         $(e.target).blur();
