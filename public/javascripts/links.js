fa868a86945b87e27f791a2c855030549f7dbe90
Matthew Garrett <mattga@gmail.com>
2009-03-22 11:28:56 -0500

    ...

--- public/javascripts/links.js
+++ public/javascripts/links.js
@@ -4,6 +4,7 @@ var Links = {
     this.links = $('links');
     
     this.links.observe('click', function(e){
+      console.log(e);
       if ($(e.target).readAttribute('href').include('#')) {
         $(e.target).fire('link:clicked', { href: $(e.target).readAttribute('href') });
         $(e.target).blur();
