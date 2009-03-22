a5fcb764fc3b042595167475e174996f5ae9f600
Matthew Garrett <mattga@gmail.com>
2009-03-22 11:32:53 -0500

    ...

--- public/javascripts/links.js
+++ public/javascripts/links.js
@@ -3,7 +3,16 @@ var Links = {
   setup: function(){
     this.links = $('links');
     
-    this.links.observe('click', function(e){
+    // this.links.observe('click', function(e){
+    //   console.log(e);
+    //   if ($(e.target).readAttribute('href').include('#')) {
+    //     $(e.target).fire('link:clicked', { href: $(e.target).readAttribute('href') });
+    //     $(e.target).blur();
+    //     e.stop();
+    //   }
+    // });
+    
+    $('add').observe('click', function(e){
       console.log(e);
       if ($(e.target).readAttribute('href').include('#')) {
         $(e.target).fire('link:clicked', { href: $(e.target).readAttribute('href') });
