57628e87e2051802114d3872593c5b4a2d12ba96
Matt Garrett <mgarrett@goldstar.com>
2019-03-17 16:42:27 -0700

    format

--- src/lib/__tests__/are-array-values-shallow-equal-test.js
+++ src/lib/__tests__/are-array-values-shallow-equal-test.js
@@ -3,17 +3,17 @@ import { areArrayValuesShallowEqual } from '../are-array-values-shallow-equal.js
 describe('areArrayValuesShallowEqual()', () => {
   describe('when called with arrays with values of shallow equality', () => {
     it('should return true', () => {
-      const [ a, b, c ] = 'abc'.split('');
-      const array1 = [{ a }, { a,b }, { a,b,c }, []];
-      const array2 = [{ a }, { a,b }, { a,b,c }, []];
+      const [a, b, c] = 'abc'.split('');
+      const array1 = [{ a }, { a, b }, { a, b, c }, []];
+      const array2 = [{ a }, { a, b }, { a, b, c }, []];
       expect(areArrayValuesShallowEqual(array1, array2)).toBe(true);
     });
   });
   describe('when called with arrays without values of shallow equality', () => {
     it('should return false', () => {
-      const [ a, b, c ] = 'abc'.split('');
-      const array1 = [{ a }, { a,b }, { a,b,c }, []];
-      const array2 = [{ a }, { a,b }, {}, []];
+      const [a, b, c] = 'abc'.split('');
+      const array1 = [{ a }, { a, b }, { a, b, c }, []];
+      const array2 = [{ a }, { a, b }, {}, []];
       expect(areArrayValuesShallowEqual(array1, array2)).toBe(false);
     });
   });
