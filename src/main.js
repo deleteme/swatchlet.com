81cb94226d5163be2f0ead1e51ef9ac676fd8fb7
Matt Garrett <mattga@gmail.com>
2019-05-11 12:28:33 -0500

    clean up

--- src/main.js
+++ src/main.js
@@ -9,32 +9,16 @@ const syncURLtoState = memoize(function _syncURLtoState(url) {
   if (newState) {
     name.set(newState.name);
     swatches.set(newState.swatches);
-    //store.set(newState);
     return true;
   }
 });
 
 if (!syncURLtoState(location.href)) {
-  console.log('redirecting to ');
   location.hash = toString({
     name: get(name),
     swatches: get(swatches)
   });
 }
-
-/*
-const syncStateToUrl = memoize(function _syncStateToUrl(state) {
-  //console.log('syncStateToUrl', state);
-  const url = renderHash(state);
-  if (url !== location.hash) {
-    location.hash = url;
-  }
-});
-
-const handleStateChange = ({ current }) => syncStateToUrl(current);
-
-store.on('state', handleStateChange);
-*/
 
 const derivedLocationHash = derived([name, swatches], ([$name, $swatches]) => {
   return renderHash({
@@ -48,14 +32,9 @@ derivedLocationHash.subscribe(hash => {
 });
 
 const handleHashChange = e => {
-  //console.log('handle hash change');
   syncURLtoState(e.newURL);
 };
 
 window.addEventListener('hashchange', handleHashChange, false);
 
-const app = new App({
-  target: document.body
-});
-//console.log('exporting app');
-export default app;
+export default new App({ target: document.body });
