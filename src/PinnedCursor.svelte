f3834f6e8819e52c091cfd639126660d3a62f554
Matt Garrett <mattga@gmail.com>
2019-05-15 12:40:51 -0500

    add spring to pinned cursor

--- src/PinnedCursor.svelte
+++ src/PinnedCursor.svelte
@@ -1,7 +1,16 @@
 <script>
+  import { spring } from 'svelte/motion';
+  let coords = spring({ left: 0, top: 0 }, {
+    stiffness: 0.2,
+    damping: 0.8
+  });
   export let left = 0;
   export let top = 0;
   export let width = 10;
+
+  $: {
+    coords.set({ left: left, top: top });
+  }
 </script>
 <style>
 .pinned-cursor {
@@ -11,9 +20,10 @@
   box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.7);
   box-sizing: border-box;
   height: 7px;
+  left: -3px;
   position: absolute;
   pointer-events: none;
-  transform: translateX(-3px) translateY(-3px);
+  top: -3px;
 }
 </style>
-<div class="pinned-cursor" style="left: {left}px; top: {top}px; width: {width + (3 * 2)}px;"></div>
+<div class="pinned-cursor" style="transform: translateX({$coords.left}px) translateY({$coords.top}px); width: {width + (3 * 2)}px;"></div>
