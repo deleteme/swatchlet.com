32e1d4eef4637fd722c8fdc46efc24edccee6371
Matt Garrett <mattga@gmail.com>
2019-05-15 12:41:37 -0500

    render rgb values in labels

--- src/PinnedRadios.svelte
+++ src/PinnedRadios.svelte
@@ -1,35 +1,58 @@
 <script>
   import { pinned } from './picker-canvas-store.js';
+  import { pickingSwatchRgb } from './store.js';
 </script>
 
 <style>
-div {
-  --golden-ratio: 1.618;
-  --side: calc((1 / var(--golden-ratio)) * 100%);
-  --margin: calc((100% - var(--side)) / 2);
-  box-sizing: border-box;
-  left: calc(var(--side) + var(--margin));
-  line-height: 4rem;
-  padding: 1rem;
-  position: absolute;
-  width: var(--margin);
-  top: var(--margin);
+label {
+  font-weight: bold;
+}
+@media screen and (min-width: 813px) {
+  div {
+    --golden-ratio: 1.618;
+    --side: calc((1 / var(--golden-ratio)) * 100%);
+    --margin: calc((100% - var(--side)) / 2);
+    box-sizing: border-box;
+    left: calc(var(--side) + var(--margin));
+    line-height: 4rem;
+    padding: 1rem;
+    position: absolute;
+    width: var(--margin);
+    top: var(--margin);
+  }
+}
+@media screen and (min-width: 0) and (max-width: 812px) {
+  div {
+    left: 45px;
+    position: absolute;
+    top: 120px;
+  }
+  label {
+    display: inline-flex;
+    padding-right: 1rem;
+  }
+  input {
+    margin-right: 0.5rem;
+  }
+  label * {
+    align-self: center;
+  }
 }
 </style>
 
 <div>
   <label>
     <input type=radio bind:group={$pinned} value='R'>
-    R
+    R: { $pickingSwatchRgb.R }
   </label>
 
   <label>
     <input type=radio bind:group={$pinned} value='G'>
-    G
+    G: { $pickingSwatchRgb.G }
   </label>
 
   <label>
     <input type=radio bind:group={$pinned} value='B'>
-    B
+    B: { $pickingSwatchRgb.B }
   </label>
 </div>
