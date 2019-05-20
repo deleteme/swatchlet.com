21fbfe78985b8ddf96b0ca5c902e037a18a74d20
Matt Garrett <mattga@gmail.com>
2019-05-20 08:52:14 -0700

    remove console.log

--- src/breakpoint-store.js
+++ src/breakpoint-store.js
@@ -1,7 +1,6 @@
 import { writable } from 'svelte/store';
 
 const isMobileMediaQuery = window.matchMedia('(min-width: 0) and (max-width: 812px)');
-console.log('isMobileMediaQuery', isMobileMediaQuery);
 
 export const isMobile = writable(isMobileMediaQuery.matches);
 
