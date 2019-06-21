053a877672b209d6754f62254e76ffe2b1f79291
Matt Garrett <mattga@gmail.com>
2019-06-20 23:00:38 -0700

    it sort of works

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -7,12 +7,12 @@
   const handleKeyUp = e => {
     if (e.code === 'Escape') cancelPicking();
   }
-  export let width = 0;
   export let targetHeight = 0;
   export let targetWidth = 0;
   export let originElementDimensions;
   export let background = '';
   let dimensions = originElementDimensions;
+  //let isMounted = false;
   /*
   target width, height
   original width, height
@@ -36,6 +36,7 @@
     { x: dimensions.offsetWidth, y: dimensions.offsetHeight },
     { damping: 0.8, stiffness: 0.15 }
   );
+  //let flyDelayFinished = false;
   $: overlayStyle = [
       `left: ${0}px`,
       `top: ${0}px`,
@@ -44,8 +45,7 @@
       `transform: scale(${$scale.x}, ${$scale.y}) translate(${$position.x}px, ${$position.y}px)`,
       `transform-origin: ${$origin.x}px ${$origin.y}px`
     ].join('; ')
-  $: {
-  }
+  //let timeout;
   onMount(() => {
     $position = { x: 0, y: 0 };
     $origin = { x: 0, y: 0 };
@@ -54,7 +54,19 @@
       x: targetWidth / dimensions.offsetWidth,
       y: targetHeight / dimensions.offsetHeight
     }
+    /*
+    timeout = setTimeout(() => {
+      flyDelayFinished = true;
+    }, 200 + 170);
+    isMounted = true;
+    */
   });
+  /*
+  onDestroy(() => {
+    clearTimeout(timeout);
+    isMounted = false;
+  });
+  */
 </script>
 <style>
 .modal {
@@ -77,20 +89,16 @@
 }
 </style>
 <svelte:window on:keyup={handleKeyUp}></svelte:window>
-{#if overlayStyle}
 <div class="overlay-overflow">
   <div
     style="{ overlayStyle }; background: { background }"
     class="overlay"
   >
   </div>
+  <div
+    class="modal"
+    transition:fly="{{delay: 100, duration: 200, x: targetWidth / 10, y: 0, opacity: 0, easing: quintOut}}"
+  >
+    <slot></slot>
+  </div>
 </div>
-{/if}
-{#if false }
-<div
-  class="modal"
-  transition:fly="{{delay: 1, duration: 200, x: width, y: 0, opacity: 1, easing: quintOut}}"
->
-  <slot></slot>
-</div>
-{/if}
